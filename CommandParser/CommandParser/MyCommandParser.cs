966da1ed3ee6e114aa244d116e5c32ed39017ec4
Rudeka <thotubox@gmail.com>
2015-09-07 00:32:55 +0300

    Initial

--- CommandParser/CommandParser/MyCommandParser.cs
+++ CommandParser/CommandParser/MyCommandParser.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Odbc;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CommandParser
 {
@@ -24,8 +21,6 @@ namespace CommandParser
         private string kValue;
         private string kKey;
 
-
-
         public MyCommandParser(string[] Input)
         {
             input = Input;
@@ -39,76 +34,39 @@ namespace CommandParser
                 {
 
                     case ping:
-                        Console.WriteLine("Pinging...");
-                        Console.Beep();
-                        loopNumber++;
+                        RunPing();
                         break;
 
                     case keyValue:
-                        for (var f = loopNumber+1; f < input.Length; f++)
-                        {
-
-
-                            if (!supportedCommands.Contains(input[f]))
-                            {
-                                kKey = input[f];
-                                kValue = "<null>";
-                                if (f + 1 < input.Length &&
-                                    !supportedCommands.Contains(input[f + 1]))
-                                {
-                                    kValue = input[f + 1];
-                                    f++;
-                                }
-                                if (f + 1 < input.Length &&
-                                    supportedCommands.Contains(input[f + 1]))
-                                {
-                                    f = input.Length;
-                                }
-                                kCommand.Add(kKey, kValue);
-                                loopNumber++;
-                            }
-
-                            if (loopNumber < input.Length &&
-                                !supportedCommands.Contains(input[loopNumber]))
-                                loopNumber++;
-
-
-
-                        }
-                        foreach (var kvp in kCommand)
-                            Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
-                        if (loopNumber < input.Length &&
-                            !supportedCommands.Contains(input[loopNumber]))
-                            loopNumber++;
+                        RunKeyValue();
                         break;
 
                     case print:
-                        for (var i = loopNumber+1; i < input.Length; i++)
-                        {
-                            if (!supportedCommands.Contains(input[i]))
-                            {
-                                Console.Write("{0} ", input[i]);
-                                loopNumber++;
-                            }
-
-                            if (supportedCommands.Contains(input[i]))
-                                i = input.Length;
-
-                        }
-                        Console.WriteLine();
-                        if (loopNumber < input.Length &&
-                            !supportedCommands.Contains(input[loopNumber]))
-                            loopNumber++;
+                        RunPrint();
                         break;
 
                     case helpA:
                     case helpB:
                     case helpC:
-                        if (!input.Contains(keyValue) &&
-                            !input.Contains(ping) &&
-                            !input.Contains(print))
-                        {
-                            Console.WriteLine(@"Set of supported commands:
+                        RunHelp();
+                        break;
+
+                    default:
+                        throw new Exception(string.Format
+                            ("Command <{0}> is not supported, use " +
+                             "CommandParser.exe /? to see a set of allowed commands",
+                                input[loopNumber]));
+                }
+            }
+        }
+
+        private void RunHelp()
+        {
+            if (!input.Contains(keyValue) &&
+                !input.Contains(ping) &&
+                !input.Contains(print))
+            {
+                Console.WriteLine(@"Set of supported commands:
 -k key value 
 shows a table of
 key1 - value1
@@ -118,20 +76,66 @@ makes a sound signal and print Pinging...
 -print print a value
 prints all text after command
 ");
-                            loopNumber = input.Length;
-                        }
+                loopNumber = input.Length;
+            }
+            loopNumber++;
+        }
+
+        private void RunPrint()
+        {
+            for (var i = loopNumber + 1; i < input.Length; i++)
+            {
+                    if (!supportedCommands.Contains(input[i]))
+                    {
+                        Console.Write("{0} ", input[i]);
                         loopNumber++;
+                    }
 
-                        break;
-
-                    default:
-                        throw new Exception(string.Format
-                            ("Command <{0}> is not supported, use " +
-                             "CommandParser.exe /? to see a set of allowed commands",
-                                input[loopNumber]));
+                    else
+                        i = input.Length;
+                
+            }
+            Console.WriteLine();
+            loopNumber++;
+        }
+        private void RunKeyValue()
+        {
+            for (var f = loopNumber + 1; f < input.Length; f++)
+            {
+                if (!supportedCommands.Contains(input[f]))
+                {
+                    kKey = input[f];
+                    kValue = "<null>";
+                    if (f + 1 < input.Length &&
+                        !supportedCommands.Contains(input[f + 1]))
+                    {
+                        kValue = input[f + 1];
+                        f++;
+                    }
+                    if (f + 1 < input.Length &&
+                        supportedCommands.Contains(input[f + 1]))
+                    {
+                        f = input.Length;
+                    }
+                    kCommand.Add(kKey, kValue);
+                    loopNumber++;
                 }
 
+                if (loopNumber < input.Length &&
+                    !supportedCommands.Contains(input[loopNumber]))
+                    loopNumber++;
             }
+            foreach (var kvp in kCommand)
+                Console.WriteLine("{0} - {1}", kvp.Key, kvp.Value);
+            if (loopNumber < input.Length &&
+                !supportedCommands.Contains(input[loopNumber]))
+                loopNumber++;
+        }
+        private void RunPing()
+        {
+            Console.WriteLine("Pinging...");
+            Console.Beep();
+            loopNumber++;
         }
     }
 }
