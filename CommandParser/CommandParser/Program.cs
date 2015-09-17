106feaa7dd222137e025b85c3bd5ddc4c8fd6b91
Rudeka <thotubox@gmail.com>
2015-09-17 12:50:34 +0300

    CorrectionsKottans

--- CommandParser/CommandParser/Program.cs
+++ CommandParser/CommandParser/Program.cs
@@ -3,40 +3,5 @@
 
 namespace CommandParser
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                if (args.Length == 0)
-                    Console.WriteLine(@"Set of supported commands:
-
--k key value 
-shows a table of
-key1 - value1
-key2 - value2
-
--ping
-makes a sound signal and print Pinging...
-
--print print a value
-prints all text after command
-");
-                else
-                {
-                    var commandParser = new MyCommandParser(args);
-                    commandParser.Run();
-
-                }
-            }
-            catch (Exception e)
-            {
-                
-                Console.WriteLine(e.Message);
-            }
-
-            
-        }
-    }
+    
 }
